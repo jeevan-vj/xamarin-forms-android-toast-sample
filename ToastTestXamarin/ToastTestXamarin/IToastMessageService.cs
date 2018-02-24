6df1d5a843bb412f3e3da9b7fbf3e418fc4bf604
Jeevan Wijerathna <jeevan90wijerathna@gmail.com>
2018-02-24 22:18:22 +0800

    initial commit

---
+++ ToastTestXamarin/ToastTestXamarin/IToastMessageService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ToastTestXamarin
+{
+    public interface IToastMessageService
+    {
+        void ShowToastMessage(string message);
+    }
+}
---
+++ ToastTestXamarin/ToastTestXamarin/MainPage.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace ToastTestXamarin
+{
+	public partial class MainPage : ContentPage
+	{
+		public MainPage()
+		{
+			InitializeComponent();
+		}
+
+	    private void ShowToast(Object sender, EventArgs args)
+	    {
+	        DependencyService.Get<IToastMessageService>().ShowToastMessage("Test Toast Message");
+	    }
+
+    }
+}
